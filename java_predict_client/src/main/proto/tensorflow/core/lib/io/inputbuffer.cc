621e63164a50bc8559451f49c0984ec1478133ac
tobe <tobeg3oogle@gmail.com>
2016-12-09 18:54:57 +0800

    Add proto tensorflow lib files

---
+++ java_predict_client/src/main/proto/tensorflow/core/lib/io/inputbuffer.cc
@@ -0,0 +1,176 @@
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/lib/io/inputbuffer.h"
+#include "tensorflow/core/lib/core/errors.h"
+
+namespace tensorflow {
+namespace io {
+
+InputBuffer::InputBuffer(RandomAccessFile* file, size_t buffer_bytes)
+    : file_(file),
+      file_pos_(0),
+      size_(buffer_bytes),
+      buf_(new char[size_]),
+      pos_(buf_),
+      limit_(buf_) {}
+
+InputBuffer::~InputBuffer() { delete[] buf_; }
+
+Status InputBuffer::FillBuffer() {
+  StringPiece data;
+  Status s = file_->Read(file_pos_, size_, &data, buf_);
+  if (data.data() != buf_) {
+    memmove(buf_, data.data(), data.size());
+  }
+  pos_ = buf_;
+  limit_ = pos_ + data.size();
+  file_pos_ += data.size();
+  return s;
+}
+
+Status InputBuffer::ReadLine(string* result) {
+  result->clear();
+  int i;
+  Status s;
+  for (i = 0;; i++) {
+    if (pos_ == limit_) {
+      // Get more data into buffer
+      s = FillBuffer();
+      if (limit_ == buf_) {
+        break;
+      }
+    }
+    char c = *pos_++;
+    if (c == '\n') {
+      // We don't append the '\n' to *result
+      return Status::OK();
+    }
+    // We don't append '\r' to *result
+    if (c != '\r') {
+      *result += c;
+    }
+  }
+  if (errors::IsOutOfRange(s) && !result->empty()) {
+    return Status::OK();
+  }
+  return s;
+}
+
+Status InputBuffer::ReadNBytes(int64 bytes_to_read, string* result) {
+  result->clear();
+  if (bytes_to_read < 0) {
+    return errors::InvalidArgument("Can't read a negative number of bytes: ",
+                                   bytes_to_read);
+  }
+  result->resize(bytes_to_read);
+  size_t bytes_read = 0;
+  Status status = ReadNBytes(bytes_to_read, &(*result)[0], &bytes_read);
+  if (bytes_read < bytes_to_read) result->resize(bytes_read);
+  return status;
+}
+
+Status InputBuffer::ReadNBytes(int64 bytes_to_read, char* result,
+                               size_t* bytes_read) {
+  if (bytes_to_read < 0) {
+    return errors::InvalidArgument("Can't read a negative number of bytes: ",
+                                   bytes_to_read);
+  }
+  Status status;
+  *bytes_read = 0;
+  while (*bytes_read < static_cast<size_t>(bytes_to_read)) {
+    if (pos_ == limit_) {
+      // Get more data into buffer.
+      status = FillBuffer();
+      if (limit_ == buf_) {
+        break;
+      }
+    }
+    // Do not go over the buffer boundary.
+    const int64 bytes_to_copy =
+        std::min<int64>(limit_ - pos_, bytes_to_read - *bytes_read);
+    // Copies buffered data into the destination.
+    memcpy(result + *bytes_read, pos_, bytes_to_copy);
+    pos_ += bytes_to_copy;
+    *bytes_read += bytes_to_copy;
+  }
+  if (errors::IsOutOfRange(status) &&
+      (*bytes_read == static_cast<size_t>(bytes_to_read))) {
+    return Status::OK();
+  }
+  return status;
+}
+
+Status InputBuffer::ReadVarint32Fallback(uint32* result) {
+  uint8 scratch = 0;
+  char* p = reinterpret_cast<char*>(&scratch);
+  size_t unused_bytes_read = 0;
+
+  *result = 0;
+  for (int shift = 0; shift <= 28; shift += 7) {
+    TF_RETURN_IF_ERROR(ReadNBytes(1, p, &unused_bytes_read));
+    *result |= (scratch & 127) << shift;
+    if (!(scratch & 128)) return Status::OK();
+  }
+  return errors::DataLoss("Stored data is too large to be a varint32.");
+}
+
+Status InputBuffer::SkipNBytes(int64 bytes_to_skip) {
+  if (bytes_to_skip < 0) {
+    return errors::InvalidArgument("Can only skip forward, not ",
+                                   bytes_to_skip);
+  }
+  int64 bytes_skipped = 0;
+  Status s;
+  while (bytes_skipped < bytes_to_skip) {
+    if (pos_ == limit_) {
+      // Get more data into buffer
+      s = FillBuffer();
+      if (limit_ == buf_) {
+        break;
+      }
+    }
+    const int64 bytes_to_advance =
+        std::min<int64>(limit_ - pos_, bytes_to_skip - bytes_skipped);
+    bytes_skipped += bytes_to_advance;
+    pos_ += bytes_to_advance;
+  }
+  if (errors::IsOutOfRange(s) && bytes_skipped == bytes_to_skip) {
+    return Status::OK();
+  }
+  return s;
+}
+
+Status InputBuffer::Seek(int64 position) {
+  if (position < 0) {
+    return errors::InvalidArgument("Seeking to a negative position: ",
+                                   position);
+  }
+  // Position of the buffer within file.
+  const int64 bufpos = file_pos_ - static_cast<int64>(limit_ - buf_);
+  if (position >= bufpos && position < file_pos_) {
+    // Seeks to somewhere inside the buffer.
+    pos_ = buf_ + (position - bufpos);
+    DCHECK(pos_ >= buf_ && pos_ < limit_);
+  } else {
+    // Seeks to somewhere outside.  Discards the buffered data.
+    pos_ = limit_ = buf_;
+    file_pos_ = position;
+  }
+  return Status::OK();
+}
+
+}  // namespace io
+}  // namespace tensorflow
