907ba30130dcb6cb9b59fb3904ccd5f85f873ffe
tobe <tobeg3oogle@gmail.com>
2016-12-26 16:00:39 +0800

    Print train auc when training

--- a8a_classifier.py
+++ a8a_classifier.py
@@ -6,11 +6,10 @@ import math
 import numpy as np
 import os
 from sklearn import metrics
-
 import tensorflow as tf
 from tensorflow.contrib.session_bundle import exporter
 
-# Define parameters
+# Define hyperparameters
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
@@ -41,7 +40,7 @@ flags.DEFINE_integer("export_version", 1, "Version number of the model")
 
 
 def main():
-  # Change the input and output for different models
+  # Change these for different models
   FEATURE_SIZE = 124
   LABEL_SIZE = 2
   TRAIN_TFRECORDS_FILE = "data/a8a_train.libsvm.tfrecords"
@@ -62,7 +61,6 @@ def main():
   if not os.path.exists(tensorboard_dir):
     os.makedirs(tensorboard_dir)
 
-  # Read TFRecords examples from filename queue
   def read_and_decode(filename_queue):
     reader = tf.TFRecordReader()
     _, serialized_example = reader.read(filename_queue)
@@ -90,7 +88,7 @@ def main():
   batch_ids = features["ids"]
   batch_values = features["values"]
 
-  # Read TFRecords file for validatioin
+  # Read TFRecords file for validation
   validate_filename_queue = tf.train.string_input_producer(
       tf.train.match_filenames_once(VALIDATE_TFRECORDS_FILE),
       num_epochs=epoch_number)
@@ -163,7 +161,7 @@ def main():
     return tf.nn.relu(full_connect(inputs, weights_shape, biases_shape,
                                    is_train))
 
-  def deep_inference(sparse_ids, sparse_values, is_train=True):
+  def dnn_inference(sparse_ids, sparse_values, is_train=True):
     with tf.variable_scope("layer1"):
       sparse_layer = sparse_full_connect(sparse_ids, sparse_values,
                                          [input_units, hidden1_units],
@@ -175,35 +173,29 @@ def main():
     with tf.variable_scope("layer3"):
       layer = full_connect_relu(layer, [hidden2_units, hidden3_units],
                                 [hidden3_units], is_train)
-
     if FLAGS.enable_dropout and is_train:
       layer = tf.nn.dropout(layer, FLAGS.dropout_keep_prob)
-
     with tf.variable_scope("output"):
       layer = full_connect(layer, [hidden3_units, output_units],
                            [output_units], is_train)
     return layer
 
-  def wide_inference(sparse_ids, sparse_values, is_train=True):
-    """
-      Logistic regression model.
-      """
+  def lr_inference(sparse_ids, sparse_values, is_train=True):
     with tf.variable_scope("logistic_regression"):
       layer = sparse_full_connect(sparse_ids, sparse_values,
                                   [input_units, output_units], [output_units])
     return layer
 
   def wide_and_deep_inference(sparse_ids, sparse_values, is_train=True):
-    return wide_inference(sparse_ids, sparse_values,
-                          is_train) + deep_inference(sparse_ids, sparse_values,
-                                                     is_train)
+    return lr_inference(sparse_ids, sparse_values, is_train) + dnn_inference(
+        sparse_ids, sparse_values, is_train)
 
   def inference(sparse_ids, sparse_values, is_train=True):
     print("Use the model: {}".format(FLAGS.model))
     if FLAGS.model == "lr":
-      return wide_inference(sparse_ids, sparse_values, is_train)
+      return lr_inference(sparse_ids, sparse_values, is_train)
     elif FLAGS.model == "dnn":
-      return deep_inference(sparse_ids, sparse_values, is_train)
+      return dnn_inference(sparse_ids, sparse_values, is_train)
     elif FLAGS.model == "wide_and_deep":
       return wide_and_deep_inference(sparse_ids, sparse_values, is_train)
     else:
@@ -237,12 +229,32 @@ def main():
     print("Unknow optimizer: {}, exit now".format(FLAGS.optimizer))
     exit(1)
 
-  with tf.device("/cpu:0"):
+  with tf.device('/cpu:0'):
     global_step = tf.Variable(0, name='global_step', trainable=False)
   train_op = optimizer.minimize(loss, global_step=global_step)
 
-  # Compute accuracy
   tf.get_variable_scope().reuse_variables()
+
+  # Define accuracy op for train data
+  train_accuracy_logits = inference(batch_ids, batch_values, False)
+  train_softmax = tf.nn.softmax(train_accuracy_logits)
+  train_correct_prediction = tf.equal(
+      tf.argmax(train_softmax, 1), batch_labels)
+  train_accuracy = tf.reduce_mean(tf.cast(train_correct_prediction,
+                                          tf.float32))
+
+  # Define auc op for train data
+  batch_labels = tf.cast(batch_labels, tf.int32)
+  sparse_labels = tf.reshape(batch_labels, [-1, 1])
+  derived_size = tf.shape(batch_labels)[0]
+  indices = tf.reshape(tf.range(0, derived_size, 1), [-1, 1])
+  concated = tf.concat(1, [indices, sparse_labels])
+  outshape = tf.pack([derived_size, LABEL_SIZE])
+  new_train_batch_labels = tf.sparse_to_dense(concated, outshape, 1.0, 0.0)
+  _, train_auc = tf.contrib.metrics.streaming_auc(train_softmax,
+                                                  new_train_batch_labels)
+
+  # Define accuracy op for validate data
   validate_accuracy_logits = inference(validate_batch_ids,
                                        validate_batch_values, False)
   validate_softmax = tf.nn.softmax(validate_accuracy_logits)
@@ -252,14 +264,7 @@ def main():
   validate_accuracy = tf.reduce_mean(tf.cast(validate_correct_prediction,
                                              tf.float32))
 
-  train_accuracy_logits = inference(batch_ids, batch_values, False)
-  train_softmax = tf.nn.softmax(train_accuracy_logits)
-  train_correct_prediction = tf.equal(
-      tf.argmax(train_softmax, 1), batch_labels)
-  train_accuracy = tf.reduce_mean(tf.cast(train_correct_prediction,
-                                          tf.float32))
-
-  # Compute auc
+  # Define auc op for validate data
   validate_batch_labels = tf.cast(validate_batch_labels, tf.int32)
   sparse_labels = tf.reshape(validate_batch_labels, [-1, 1])
   derived_size = tf.shape(validate_batch_labels)[0]
@@ -267,8 +272,8 @@ def main():
   concated = tf.concat(1, [indices, sparse_labels])
   outshape = tf.pack([derived_size, LABEL_SIZE])
   new_validate_batch_labels = tf.sparse_to_dense(concated, outshape, 1.0, 0.0)
-  _, auc_op = tf.contrib.metrics.streaming_auc(validate_softmax,
-                                               new_validate_batch_labels)
+  _, validate_auc = tf.contrib.metrics.streaming_auc(validate_softmax,
+                                                     new_validate_batch_labels)
 
   # Define inference op
   sparse_index = tf.placeholder(tf.int64)
@@ -284,19 +289,20 @@ def main():
   # Initialize saver and summary
   checkpoint_file = checkpoint_dir + "/checkpoint.ckpt"
   steps_to_validate = FLAGS.steps_to_validate
-  init_op = tf.initialize_all_variables()
-  tf.scalar_summary('loss', loss)
-  tf.scalar_summary('accuracy', validate_accuracy)
-  tf.scalar_summary('auc', auc_op)
+  tf.scalar_summary("loss", loss)
+  tf.scalar_summary("train_accuracy", train_accuracy)
+  tf.scalar_summary("train_auc", train_auc)
+  tf.scalar_summary("validate_accuracy", validate_accuracy)
+  tf.scalar_summary("validate_auc", validate_auc)
   saver = tf.train.Saver()
   keys_placeholder = tf.placeholder("float")
   keys = tf.identity(keys_placeholder)
 
-  # Create session to run graph
+  # Create session to run
   with tf.Session() as sess:
     summary_op = tf.merge_all_summaries()
     writer = tf.train.SummaryWriter(tensorboard_dir, sess.graph)
-    sess.run(init_op)
+    sess.run(tf.initialize_all_variables())
     sess.run(tf.initialize_local_variables())
 
     if mode == "train":
@@ -316,20 +322,21 @@ def main():
           _, loss_value, step = sess.run([train_op, loss, global_step])
 
           if step % steps_to_validate == 0:
-            train_accuracy_value, validate_accuracy_value, auc_value, summary_value = sess.run(
-                [train_accuracy, validate_accuracy, auc_op, summary_op])
+            train_accuracy_value, train_auc_value, validate_accuracy_value, auc_value, summary_value = sess.run(
+                [train_accuracy, train_auc, validate_accuracy, validate_auc,
+                 summary_op])
             end_time = datetime.datetime.now()
             print(
-                "[{}] Step: {}, loss: {}, train accuracy: {}, validate accuracy: {}, auc: {}".format(
+                "[{}] Step: {}, loss: {}, train_acc: {}, train_auc: {}, valid_acc: {}, valid_auc: {}".format(
                     end_time - start_time, step, loss_value,
-                    train_accuracy_value, validate_accuracy_value, auc_value))
+                    train_accuracy_value, train_auc_value,
+                    validate_accuracy_value, auc_value))
 
             writer.add_summary(summary_value, step)
             saver.save(sess, checkpoint_file, global_step=step)
             start_time = end_time
       except tf.errors.OutOfRangeError:
         print("Done training after reading all data")
-
         print("Exporting trained model to {}".format(FLAGS.model_path))
         model_exporter = exporter.Exporter(saver)
         model_exporter.init(
@@ -347,8 +354,6 @@ def main():
             })
         model_exporter.export(FLAGS.model_path,
                               tf.constant(FLAGS.export_version), sess)
-        print 'Done exporting!'
-
       finally:
         coord.request_stop()
 
@@ -410,48 +415,45 @@ def main():
           feature_num += 1
         ins_num += 1
 
-      # Restore wights from model file
       ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
       if ckpt and ckpt.model_checkpoint_path:
         print("Use the model {}".format(ckpt.model_checkpoint_path))
         saver.restore(sess, ckpt.model_checkpoint_path)
-
-        prediction, prediction_softmax = sess.run(
-            [inference_op, inference_softmax],
-            feed_dict={sparse_index: feature_index,
-                       sparse_ids: feature_ids,
-                       sparse_values: feature_values,
-                       sparse_shape: [ins_num, FEATURE_SIZE]})
-
-        end_time = datetime.datetime.now()
-        print("[{}] Inference result: {}".format(end_time - start_time,
-                                                 prediction))
-
-        # Compute accuracy
-        label_number = len(labels)
-        correct_label_number = 0
-        for i in range(label_number):
-          if labels[i] == prediction[i]:
-            correct_label_number += 1
-        accuracy = float(correct_label_number) / label_number
-
-        # Compute auc
-        expected_labels = np.array(labels)
-        predict_labels = prediction_softmax[:, 0]
-        fpr, tpr, thresholds = metrics.roc_curve(expected_labels,
-                                                 predict_labels,
-                                                 pos_label=0)
-        auc = metrics.auc(fpr, tpr)
-        print("For inference data, accuracy: {}, auc: {}".format(accuracy,
-                                                                 auc))
-
-        # Save inference result into file
-        np.savetxt(inference_result_file_name, prediction, delimiter=",")
-        print("Save result to file: {}".format(inference_result_file_name))
-
       else:
         print("No model found, exit now")
         exit(1)
+
+      prediction, prediction_softmax = sess.run(
+          [inference_op, inference_softmax],
+          feed_dict={sparse_index: feature_index,
+                     sparse_ids: feature_ids,
+                     sparse_values: feature_values,
+                     sparse_shape: [ins_num, FEATURE_SIZE]})
+
+      end_time = datetime.datetime.now()
+      print("[{}] Inference result: {}".format(end_time - start_time,
+                                               prediction))
+
+      # Compute accuracy
+      label_number = len(labels)
+      correct_label_number = 0
+      for i in range(label_number):
+        if labels[i] == prediction[i]:
+          correct_label_number += 1
+      accuracy = float(correct_label_number) / label_number
+
+      # Compute auc
+      expected_labels = np.array(labels)
+      predict_labels = prediction_softmax[:, 0]
+      fpr, tpr, thresholds = metrics.roc_curve(expected_labels,
+                                               predict_labels,
+                                               pos_label=0)
+      auc = metrics.auc(fpr, tpr)
+      print("For inference data, accuracy: {}, auc: {}".format(accuracy, auc))
+
+      # Save inference result into file
+      np.savetxt(inference_result_file_name, prediction, delimiter=",")
+      print("Save result to file: {}".format(inference_result_file_name))
 
 
 if __name__ == "__main__":
